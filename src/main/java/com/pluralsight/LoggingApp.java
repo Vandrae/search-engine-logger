fe791916c3a2f87ed7bd7132579ce23e4d2af561
Gabe <Carrollgbusiness@gmail.com>
2026-04-22 10:09:21 -0500

    First Commit

--- src/main/java/com/pluralsight/LoggingApp.java
+++ src/main/java/com/pluralsight/LoggingApp.java
@@ -1,4 +1,53 @@
 package com.pluralsight;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.Scanner;
+
 public class LoggingApp {
+    public static void main(String[] args) {
+        //scanner so they can search for terms
+        Scanner input= new Scanner(System.in);
+
+        System.out.println("""
+      What do you want to do?
+          1) Search for a Term
+          2) Read Action Log
+          0) Exit the dang app
+      """);
+        int userAction = input.nextInt();
+
+        if (userAction==1) {
+            System.out.println();
+        } else if (userAction == 2) {
+            System.out.println();
+        }else if (userAction == 0) {
+            System.out.println("Goodbye!");
+            System.exit(0);
+        }
+
+
+        try {
+            FileWriter writer = new FileWriter("src/main/resources/logs.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+
+        }catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+    //method to log actions of the user
+    //write to a file named user-actions.txt
+    //actions logged should be
+    //launching the application and when
+    //searching for a term and when
+    //exiting the application and when
+
 }
+
+
+
+
+
+
+
+
